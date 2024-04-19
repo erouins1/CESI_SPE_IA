a7524601412e8bdac37ace032b34de00cd3664b7
BenoitCesi <benoit.desbourdieux@viacesi.fr>
2024-04-19 12:56:33 +0200

    Created filter structure in sidebar for shiny app

--- app_script.R
+++ app_script.R
@@ -1,13 +1,24 @@
 # Description: This script contains the code for the shiny app
 # Import of libraries
 library(shiny)
-library(bslib)
 library(shinydashboard)
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
   dashboardHeader(title = "Basic dashboard"),
-  dashboardSidebar(),
+  dashboardSidebar(
+    # Sidebar content
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    ),
+    sliderInput("MaternalAgeSlider", "Maternal Age", min = 14, max = 60, value = 30),
+    sliderInput("MaternalWeightSlider", "Maternal Weight", min = 40, max = 150, value = 70),
+    sliderInput("MaternalHeightSlider", "Maternal Height", min = 120, max = 230, value = 170),
+    selectInput("MaternalParity", "Maternal Parity", choices = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)),
+    selectInput("ChildGender",  "Child Gender", choices = c("Male", "Female")),
+    sliderInput("GestationalAgeSlider", "Gestational Age", min = 120, max = 320, value = 220),
+  ),
   dashboardBody(
     fluidRow(
       box(plotOutput("plot1", height = 250)),
@@ -22,6 +33,17 @@ ui <- dashboardPage(
     )
   )
 )
+
+# Define the prediction model and the data outputed
+model <- function(input) {
+  # Define the model
+  model <- lm(mpg ~ wt, data = mtcars)
+
+  # Predict the value
+  prediction <- predict(model, newdata = data.frame(wt = input))
+
+  return(prediction)
+}
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
