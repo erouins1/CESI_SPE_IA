d83b01b7d15f74726ad9fb6ca8d01d356fd8375d
BenoitCesi <benoit.desbourdieux@viacesi.fr>
2024-04-22 13:23:16 +0200

    Created first integration of plotting the models results and exporting models in files

--- app_script.R
+++ app_script.R
@@ -1,48 +1,86 @@
-# Description: This script contains the code for the shiny app
-# Import of libraries
 library(shiny)
 library(shinydashboard)
+library(plotly)  # Load plotly for interactive plots
 
-# Define UI for application that draws a histogram
+predictFoetalWeight <- function(modelName, Maternal_age, Maternal_weight, Maternal_height, Parity, Sex, Gestational_age) {
+  model <- readRDS(modelName)
+
+  prediction <- numeric(Gestational_age)
+  for (i in 1:Gestational_age) {
+    data <- data.frame(
+      Maternal_age = as.numeric(Maternal_age),
+      Maternal_weight = as.numeric(Maternal_weight),
+      Maternal_height = as.numeric(Maternal_height),
+      Parity = as.numeric(Parity),
+      Sex = as.factor(Sex),
+      Gestational_age = i
+    )
+    prediction[i] <- predict(model, newdata = data)
+  }
+  return(prediction)
+}
+
 ui <- dashboardPage(
   dashboardHeader(title = "Foetal Weight Prediction"),
   dashboardSidebar(
     tags$head(
       tags$style(HTML("
-        /* CSS to center the title of all boxes in the sidebar */
         .sidebar .box-header .box-title {
           text-align: center;
           display: block;
         }
-
         .form-group > label {
-          color: black;  /* Force label color to black */
-          visibility: visible;  /* Force labels to be visible */
-          display: block;  /* Ensure labels are not set to display:none */
-          text-align: center;  /* Center the labels */
+          color: black;
+          visibility: visible;
+          display: block;
+          text-align: center;
         }
       "))
     ),
     box(
       width = "100%",
       title = "Input Parameters",
-      sliderInput("MaternalAgeSlider", "Maternal Age (years)", min = 14, max = 60, value = 37),
-      sliderInput("MaternalWeightSlider", "Maternal Weight (kg)", min = 40, max = 150, value = 95),
-      sliderInput("MaternalHeightSlider", "Maternal Height (cm)", min = 120, max = 230, value = 175),
+      sliderInput("MaternalAgeSlider", "Maternal Age (years)", min = 14, max = 60, value = 30),
+      sliderInput("MaternalWeightSlider", "Maternal Weight (kg)", min = 40, max = 150, value = 70),
+      sliderInput("MaternalHeightSlider", "Maternal Height (cm)", min = 120, max = 230, value = 165),
       selectInput("MaternalParity", "Maternal Parity", choices = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)),
       selectInput("ChildGender",  "Child Gender", choices = c("Male", "Female")),
-      sliderInput("GestationalAgeSlider", "Gestational Age (days)", min = 120, max = 320, value = 220),
+      sliderInput("GestationalAgeSlider", "Gestational Age (days)", min = 120, max = 320, value = 270),
     )
   ),
   dashboardBody(
-
+    fluidRow(
+      box(width = 12, plotlyOutput("plot"))
+    )
   )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  output$plot <- renderPlotly({
+    Maternal_age <- input$MaternalAgeSlider
+    Maternal_weight <- input$MaternalWeightSlider
+    Maternal_height <- input$MaternalHeightSlider
+    Parity <- input$MaternalParity
+    Sex <- input$ChildGender
+    Gestational_age <- input$GestationalAgeSlider
 
+    prediction <- predictFoetalWeight("./Models/polynomial_model.rds", Maternal_age, Maternal_weight, Maternal_height, Parity, Sex, Gestational_age)
+
+    gest_ages <- 1:Gestational_age
+    valid_indices <- prediction > 0
+    filtered_predictions <- prediction[valid_indices]
+    filtered_ages <- gest_ages[valid_indices]
+
+    if (length(filtered_predictions) == 0) {
+      return(NULL)
+    }
+
+    plot_ly(data = data.frame(Gestational_Age = filtered_ages, Foetal_Weight = filtered_predictions), x = ~Gestational_Age, y = ~Foetal_Weight, type = 'scatter', mode = 'lines+markers',
+            text = ~paste("Gestational Age:", Gestational_Age, "days<br>Weight:", Foetal_Weight, "grams"), hoverinfo = "text") %>%
+      layout(title = "Foetal Weight Prediction by Gestational Age",
+             xaxis = list(title = "Gestational Age (days)"),
+             yaxis = list(title = "Foetal Weight (grams)"))
+  })
 }
 
-# Run the application
 shinyApp(ui = ui, server = server)
